4e7ccb417ad170299e773262772e7a5f38796726
joaopauloantunes <jpantunesdesouza@gmail.com>
2020-09-22 20:40:03 -0300

    Deixado código mais limpo e intuitivo

--- src/jexporttospreadsheet/JExport.java
+++ src/jexporttospreadsheet/JExport.java
@@ -38,78 +38,35 @@ public class JExport {
     public void export(String fileName, String[] columnsName, List<String[]> rows) throws WriteException, IOException {
         WritableWorkbook spreadSheet = Workbook.createWorkbook(new File(fileName + ".xls"));
         
-        // Adicionando o nome da aba
         WritableSheet tab = spreadSheet.createSheet(fileName, 0);
-
-        // Cor de fundo das celular
+        
         Colour bckcolor = Colour.GREEN;
-        WritableCellFormat cellFormat = new WritableCellFormat();
-        cellFormat.setBackground(bckcolor);
-
-        // Cor e tipo de fonte
+        
         WritableFont font = new WritableFont(WritableFont.ARIAL);
         font.setColour(Colour.WHITE);
+        
+        WritableCellFormat cellFormat = new WritableCellFormat();
+        cellFormat.setBackground(bckcolor);
         cellFormat.setFont(font);
 
-        // Write the Header to the excel file
         for (int i = 0; i < columnsName.length; i++) {
-            Label label = new Label(i, 0, columnsName[i]);
-            tab.addCell(label);
+            Label labelHeader = new Label(i, 0, columnsName[i]);
+            tab.addCell(labelHeader);
             
-            WritableCell cell = tab.getWritableCell(i, 0);
-            cell.setCellFormat(cellFormat);
+            WritableCell cellHeader = tab.getWritableCell(i, 0);
+            cellHeader.setCellFormat(cellFormat);
         }
         
         for(int i = 0; i < rows.size(); i++) {
             String[] row = rows.get(i);
                     
             for(int j = 0; j < row.length; j++) {
-                Label label = new Label(j, i + 1, row[j]);
-                tab.addCell(label);
+                Label labelContent = new Label(j, i + 1, row[j]);
+                tab.addCell(labelContent);
             }
         }
-
-        //for (int linha = 1; linha < 10; linha++) {
-            /*jxl.write.Number number = new jxl.write.Number(0, linha, numeroAleatorio.nextInt(2000));
-            tab.addCell(number);*/
-
-            /*Label label = new Label(1, linha, "José da Silva");
-            tab.addCell(label);*/
-
-            /*Date data = new Date();
-            DateFormat customDateFormat = new DateFormat("dd MMM yyyy hh:mm:ss");
-            
-            WritableCellFormat dateFormat = new WritableCellFormat(customDateFormat);
-            DateTime dateCell = new DateTime(4, linha, data, dateFormat);
-            tab.addCell(dateCell);*/
-        //}
 
         spreadSheet.write();
         spreadSheet.close();
-        
-        /*String contentThead = "";
-        String contentTbody = "";
-        String contentTr = "";
-        Formatter file = null;
-        
-        file = new Formatter(fileName + ".xlsx");
-
-        for(String col : columnsName) {
-            contentThead += "\t\t\t\t<th style='background:#083923; color: #fff; text-align: center'>" + col + "</th>\n";
-        }
-        writeInFile(file, "<div>\n\t<table class='table table-hover'>\n\t\t<thead>\n\t\t\t<tr height='height: 30px;'>\n" + contentThead + "\t\t\t</tr>\n\t\t</thead>\n");
-
-        for(String[] row : content) {
-            contentTr = "";
-
-            for(String col : row) {
-                contentTr += "\t\t\t\t<td style='border-bottom: 2px solid #878484; vertical-align: middle;'>" + col + "</td>\n";
-            }
-            contentTbody += "\t\t\t<tr>\n" + contentTr + "\t\t\t</tr>\n";
-        }
-        writeInFile(file, "\t\t<tbody>\n" + contentTbody + "\t\t</tbody>\n\t</table>\n");
-        writeInFile(file, "</div>\n<div style='page-break-before: always;'></div>");
-        file.close();
-*/
     }
 }
